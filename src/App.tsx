06ad433a41569387e290d716d6b6bd7c3b31b0ed
aarnav <aarnavbos@gmail.com>
2021-02-16 17:28:28 +0100

    init landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import "./index.css";
 import { createBrowserHistory } from "history";
 import { Route, Router, Switch } from "react-router-dom";
 import RecipeListPage from "./pages/recipe/ListPage";
+import LandingPage from "./pages/LandingPage";
 
 const App: React.FC = () => {
   const history = createBrowserHistory();
   return (
     <Router history={history}>
       <Switch>
-        <Route exact path="/" component={RecipeListPage} />
-        <Route exact path="/recipes" />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/recipes" component={RecipeListPage} />
       </Switch>
     </Router>
   );
