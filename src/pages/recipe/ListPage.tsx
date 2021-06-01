db57d785443a5a1dae3122eb420dcebaa94adf37
aarnav <aarnavbos@gmail.com>
2021-06-01 17:15:32 +0100

    Initialize Filterbar toggle on Mobile

--- src/pages/recipe/ListPage.tsx
+++ src/pages/recipe/ListPage.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { RecipeCard, Navbar, Footer } from "../../components";
 import useIsMobile from "../../hooks/isMobile";
-import { useRecipesQuery, useRecipeFilterQuery } from "../../graphql";
+import {
+  useRecipesQuery,
+  useRecipeFilterQuery,
+  RecipesQuery,
+} from "../../graphql";
 import { Loading } from "../../components";
 import InfiniteScroll from "react-infinite-scroll-component";
 
@@ -14,6 +18,7 @@ const RecipeListPage = () => {
   const { loading: loadingFilter, data: filterData } = useRecipeFilterQuery();
   const isMobile = useIsMobile();
   const [currentFilters, setCurrentFilters] = useState<any>({});
+  const [toggle, setToggle] = useState(false);
   useEffect(() => {
     refetch({ filter: currentFilters });
   }, [currentFilters, refetch]);
@@ -26,13 +31,65 @@ const RecipeListPage = () => {
   const { filter } = filterData;
   return (
     <>
-      {!isMobile && <Navbar />}
+      {!isMobile ? (
+        <Navbar />
+      ) : (
+        <div
+          onClick={() => {
+            setToggle((prevState) => !prevState);
+          }}
+        >
+          TOGGLE TOGGLE
+        </div>
+      )}
+
       <div className="flex pt-10 items-start">
         {/* SIDEBAR
           TODO: FIX not showing full filterbar on scroll cause sticky
           */}
-        {!isMobile && (
-          <div className="py-12 top-12 w-1/10 pl-10">
+        {isMobile ? (
+          <div
+            className={`py-12 pl-10 ${
+              toggle ? "filterBar_fadeIn" : "filterBar_fadeOut"
+            }`}
+          >
+            <h1 className="text-2xl">Filter</h1>
+            {filter.map((item: any) => (
+              <div className="pt-5">
+                <h1 className="text-xl text-gray-600 mb-2">{item.title}</h1>
+                {item.options.map(
+                  (option: { title: string; value: string }) => {
+                    const isSelected =
+                      currentFilters[item.name] === option.value;
+                    return (
+                      <div className="text-lg mb-1 cursor-pointer">
+                        <h3
+                          className={
+                            isSelected ? "text-black" : "text-gray-600"
+                          }
+                          onClick={() => {
+                            setCurrentFilters((prevState: any) => {
+                              const state = { ...prevState };
+                              if (!isSelected) {
+                                state[item.name] = option.value;
+                              } else {
+                                delete state[item.name];
+                              }
+                              return state;
+                            });
+                          }}
+                        >
+                          {option.title}
+                        </h3>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="py-12 top-12 w-1/10 pl-10 ">
             <h1 className="text-2xl">Filter</h1>
             {filter.map((item: any) => (
               <div className="pt-5">
@@ -74,7 +131,7 @@ const RecipeListPage = () => {
           <InfiniteScroll
             dataLength={recipes?.length ?? 0}
             hasMore={hasMore}
-            loader={<h4>Loading...</h4>}
+            loader={<></>}
             next={() => {
               fetchMore({
                 variables: {
@@ -82,18 +139,22 @@ const RecipeListPage = () => {
                   after: recipes[recipes.length - 1]?.cursor || "",
                   first: 10,
                 },
-                updateQuery: (prev, { fetchMoreResult }) => {
+                updateQuery: (prev, next) => {
+                  const fetchMoreResult = next.fetchMoreResult as
+                    | RecipesQuery
+                    | undefined;
                   if (!fetchMoreResult) {
                     return prev;
                   }
-                  //@ts-ignore
-                  const newEdges = fetchMoreResult.allRecipes.edges;
-                  //@ts-ignore
-                  const pageInfo = fetchMoreResult.allRecipes.pageInfo;
+                  const newEdges = fetchMoreResult?.allRecipes?.edges;
+                  const pageInfo = fetchMoreResult?.allRecipes?.pageInfo;
                   return {
                     allRecipes: {
-                      //@ts-ignore
-                      edges: [...prev.allRecipes.edges, ...newEdges],
+                      edges: [
+                        ...((prev as RecipesQuery | undefined)?.allRecipes
+                          ?.edges || []),
+                        ...(newEdges || []),
+                      ],
                       pageInfo,
                     },
                   };
