d30204852f147f8cb1434a429000204bdc1e0514
aarnav <aarnavbos@gmail.com>
2021-06-07 22:19:20 +0100

    Update tailwind config for white listing conditionals

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,7 +2,31 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   // purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  purge: [],
+  purge: {
+    options: {
+      safelist: [
+        "overflow-hidden",
+        "order-last",
+        "fixed",
+        "gap-20",
+        "rounded-2xl",
+        "rounded-3xl",
+        "relative",
+        "z-10",
+        "h-18",
+        "h-20",
+        "h-16",
+        "h-20",
+        "grid-cols-1",
+        "md:grid-cols-3",
+        "grid-cols-7",
+        "md:grid-cols-2",
+        "grid-cols-2",
+        "sm:grid-cols-4",
+        "lg:grid-cols-2",
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
