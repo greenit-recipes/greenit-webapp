443e08ea5f22bd7e790f16b0917665cca58be0b7
KintsugiCode <liam.going@code.berlin>
2021-06-17 13:39:19 +0200

    [fix] Aucune spelled wrong

--- src/components/layout/Empty.tsx
+++ src/components/layout/Empty.tsx
@@ -6,7 +6,7 @@ export const Empty: React.FC = () => {
     <div className="flex flex-col">
       <img src={emptyIcon} className="h-36 w-36 lg:h-48 lg:w-48 self-center" />
       <h1 className="text-xl lg:text-3xl" style={{ color: "#535353" }}>
-        Accune recette trouvée
+        Aucune recette trouvée
       </h1>
     </div>
   )
