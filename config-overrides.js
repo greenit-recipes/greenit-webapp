4c3bf226e0ff272a9841acec14742da5f24ca26a
Zackautocracy <codeautocracy@gmail.com>
2022-06-01 16:43:57 +0200

    removing proptypes in bundle

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,21 @@
 const {override, addBabelPlugin, disableEsLint, addWebpackPlugin} = require("customize-cra");
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = override(
-    disableEsLint(),
-    // addWebpackPlugin(new BundleAnalyzerPlugin()),
-    //Babel Plugins
-    addBabelPlugin(["transform-imports", {
-        "lodash": {
-            "transform": "lodash/${member}",
-            "preventFullImport": true
-        }
-    }])
+  disableEsLint(),
+  addWebpackPlugin(new BundleAnalyzerPlugin()),
+  //Babel Plugins
+  addBabelPlugin([
+    "transform-imports", {
+    "lodash": {
+      "transform": "lodash/${member}",
+      "preventFullImport": true
+    }
+  }]),
+  addBabelPlugin([
+    "transform-react-remove-prop-types",
+    {
+      "removeImport": true
+    }
+  ]),
 )
