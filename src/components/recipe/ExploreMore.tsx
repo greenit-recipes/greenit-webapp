7415ea2537645edc4a954ba9c372bc3fad60ad47
Florian <florian@MacBook-Pro-de-Florian.local>
2022-06-24 18:08:15 +0200

    bug beta v6 - css recipe

--- src/components/recipe/ExploreMore.tsx
+++ src/components/recipe/ExploreMore.tsx
@@ -8,11 +8,12 @@ interface ExploreMoreProps {
 
 export const ExploreMore: React.FC<ExploreMoreProps> = ({ filter }) => {
   return (
-    <Link to={`${RouteName.recipes}?${filter}`}>
-      <div
-        className="ml-1 mr-5 h-96 md:h-84 w-52 bg-white rounded-2xl self-center relative |
-                  transform sm:hover:scale-105 ease-linear relative transition-all duration-150"
-      >
+    <Link
+      to={`${RouteName.recipes}?${filter}`}
+      className="ml-1 mr-5 h-80 w-52 bg-white rounded-2xl self-center relative |
+    transform sm:hover:scale-105 ease-linear relative transition-all duration-150"
+    >
+      <div>
         <i className="absolute top-[40%] left-[40%] text-5xl bx bx-right-arrow-alt"></i>
         <p className="absolute top-[52%] left-[30%] text-lg font-semibold">
           Explorer plus
