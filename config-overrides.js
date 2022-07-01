125ca0e5490e50ec67066d2aaad84f4f8f6556b6
Zackautocracy <codeautocracy@gmail.com>
2022-07-01 04:50:34 +0200

    generate test recipe

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,19 @@
 const {override, addBabelPlugin, disableEsLint, addWebpackPlugin} = require("customize-cra");
+const PrerenderSPAPlugin = require('@dreysolano/prerender-spa-plugin');
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+const path = require('path');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = override(
   disableEsLint(),
   // addWebpackPlugin(new BundleAnalyzerPlugin()),
+  addWebpackPlugin(new PrerenderSPAPlugin({
+    routes: ['/', '/recettes/deodorant-solide-facile'],
+    staticDir: path.join(__dirname, 'build'),
+    renderer: new Renderer({
+      renderAfterTime: 5000
+    }),
+  })),
   //Babel Plugins
   addBabelPlugin([
     "transform-imports", {
